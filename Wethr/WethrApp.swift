272a650848022eb21b9ea58cc7eea061f6ec3781
JayVnl <jorrit.v@outlook.com>
2022-04-08 14:13:24 +0200

    Initial Commit

---
+++ Wethr/WethrApp.swift
@@ -0,0 +1,17 @@
+//
+//  WethrApp.swift
+//  Wethr
+//
+//  Created by Jorrit Verstappen on 08/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct WethrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
